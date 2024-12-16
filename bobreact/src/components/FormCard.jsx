510660f80e7c5a8016040c0db434887f9cc0a513
Elisha Adeboye <y.adeboye@gmail.com>
2024-12-16 16:42:43 +0100

    refactored old code

--- bobreact/src/components/FormCard.jsx
+++ bobreact/src/components/FormCard.jsx
@@ -5,14 +5,20 @@ const FormCard = () => {
         firstName: "",
         lastName: "",
         occupation: "",
-        textArea: ""
+        textArea: "",
+        email: "",
+        password: "",
+        employmentStatus: "",
+        dietaryRestrictions: true
     })
 
     const handleChange = (event) => {
         setFormData((details) => {
+            const {name, value, type, checked } = event.target
+
             return {
                 ...details,
-                [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         } )
     }
@@ -21,18 +27,65 @@ const FormCard = () => {
   return (
     <div className='profilediv'>
         <h2>Practicing React Forms</h2>
-        <input type="text" placeholder='Firstname' name="firstName" 
-                onChange={handleChange} value={formData.firstName} /> <br />     
+        <label htmlFor="firstName">First Name: </label>
+        <input id="firstName" type="text" placeholder='First name' name="firstName" 
+                onChange={handleChange} value={formData.firstName} /> <br />
+
+        <label htmlFor="lastName">Last Name: </label>
         <input type="text" placeholder='Lastname' name="lastName"       
                 onChange={handleChange} value={formData.lastName} /> <br />
+
+        <label htmlFor="occupation">Occupation: </label>
         <input type="text" placeholder='Occupation' name="occupation" 
-                onChange={handleChange} value={formData.occupation}/> <br /><br />
+                onChange={handleChange} value={formData.occupation}/> <br />
+
+        <label htmlFor="email">Email: </label>
+        <input id="email" onChange={handleChange} type="email" name="email" placeholder="joe@schmoe.com" /> <br />
+
+        <label htmlFor="password">Password: </label>
+        <input id="password" onChange={handleChange} defaultValue="password123" type="password" name="password" /> <br />
+
+        <label htmlFor="textArea">Text Area: </label> <br />
         <textarea name="textArea" id="textArea" cols="30" rows="10"
                 onChange={handleChange} value={formData.textArea} />
+
+        <fieldset>
+          <legend>Employment Status: </legend>
+            <label>
+                <input type="radio" name="employmentStatus" />
+                Unemployed
+            </label> <br />
+            <label>
+                <input type="radio" name="employmentStatus" />
+                Part-time
+            </label> <br />
+            <label>
+                <input type="radio" name="employmentStatus" />
+                Full-time
+            </label>
+        </fieldset>
+
+        <fieldset>
+            <legend>Dietary restrictions:</legend>
+            <label>
+                <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+                Kosher
+            </label>
+            <label>
+                <input type="checkbox" name="dietaryRestrictions" value="vegan" />
+                Vegan
+            </label>
+            <label>
+                <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="gluten-free" />
+                Gluten-free
+            </label>
+        </fieldset>
+
 
         <p>Firstname: {formData.firstName}</p>
         <p>lastname: {formData.lastName}</p>
         <p>Occupation: {formData.occupation}</p>
+        
     </div>
   )
 }
